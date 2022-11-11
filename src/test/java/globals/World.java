2cfc743302a7a1d1e1ef6e6b1419d6f0a4f2255e
ex020520 <j.schenk@hollandcasino.nl>
2022-11-11 08:43:44 +0100

    Push to gitlab

---
+++ src/test/java/globals/World.java
@@ -0,0 +1,38 @@
+package globals;
+
+import dataentities.Customer;
+import helpers.DateHelper;
+import io.cucumber.java.Scenario;
+import java.util.UUID;
+import lombok.Data;
+import org.openqa.selenium.WebDriver;
+
+/******************************
+ * The World class contains all objects that are shared between different steps in Cucumber scenarios
+ * It's injected into the stepdefinitions classes using a mechanism called dependency injection, by
+ * means of the cucumber-picocontainer library. For more information as well as some examples, see
+ * http://www.thinkcode.se/blog/2017/04/01/sharing-state-between-steps-in-cucumberjvm-using-picocontainer
+ *
+ * This will ONLY WORK if cucumber-picocontainer is added as maven-dependency in pom.xml
+ */
+
+@Data
+public class World {
+
+    /******************************
+     * Browser variables
+     */
+    public static WebDriver browser;
+    public String runnerLocation;
+
+    /******************************
+     * Test-related configuration
+     */
+    public Scenario scenario;
+
+    /******************************
+     * Data-related configuration
+     */
+
+    public Customer customer;
+}
